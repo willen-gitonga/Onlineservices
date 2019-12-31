960306fbcb690dc5f9985abc01ec7e48308d81f2
savannah8 <willengitonga.wg@gmail.com>
2019-12-31 09:20:17 +0300

    changed the time

--- static/js/time.js
+++ static/js/time.js
@@ -1,5 +1,5 @@
  // Set the date we're counting down to
- var countDownDate = new Date("Jan 04, 2020 23:59:59").getTime();
+ var countDownDate = new Date("Jan 02, 2020 23:59:59").getTime();
         
  // Update the count down every 1 second
  var x = setInterval(function() {
