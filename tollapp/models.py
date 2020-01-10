3c34e9e3621d8c2a84f54e2c63d340ae15af3fc3
savannah8 <willengitonga.wg@gmail.com>
2020-01-10 14:53:39 +0300

    payment

--- tollapp/models.py
+++ tollapp/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 
 class Billing(models.Model):
     PAY_CHOICES = [
+        ('Card', 'Card'),
+        ('Paypal', 'Paypal'),
         ('Skrill', 'Skrill'),
         ('Neteller', 'Neteller'),
         ('Perfect money', 'Perfect money'),
